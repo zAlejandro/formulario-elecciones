d6865760d21695fbeb5ad140c7b9a045abcd3ef3
zAlejandro <alejandromolina651@gmail.com>
2025-05-15 20:35:10 -0400

    netlify intento 2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { HomeComponent} from './app.home.component';
 import {RegistroVotoComponent} from './registro-voto/registro-voto.component';
 
 export const routes: Routes = [
-    {path: '', component: HomeComponent},
+    {path: 'home', component: HomeComponent},
     {path: 'registro', component: RegistroVotoComponent}
 ];
